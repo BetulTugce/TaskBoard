4ddbc7ea351a497b92ff6999affd9bdcf51601b7
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2025-01-04 03:05:23 +0300

    connectionstring tanimlandi ve paketler kurularak migration islemi tamamlandi.

---
+++ Infrastructure/TaskBoard.Persistence/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskBoard.Persistence.Contexts;
+
+namespace TaskBoard.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TaskBoardDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
